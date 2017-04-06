53b993d94678cc7ce1bdcb6fd46e6df4c07b5d47
Hannah Hall <hannahkhall13@gmail.com>
2017-04-06 15:59:31 -0500

    finish

--- boybands.js
+++ boybands.js
@@ -10,11 +10,13 @@ var vegetables = [
 	"Kale",
 	"Zucchini",
 	"Broccoli",
-	"Squash"
+	"Squash",
+	"Tomato"
 ];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+// var loopCount = 5;
+var loopCount = bands.length >vegetables.length ? bands.length : vegetables.length;
 
 // Keep track of which band we're on in the loop
 var currentBand = "";
@@ -24,53 +26,18 @@ var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
 var bandElement = document.getElementById("boy-bands");
-bandElement
 
 // Get a reference to the appropriate DOM element for vegetables
 var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-
-
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 	currentBand= bands[loopTracker];
-	console.log("Loop Number, Band:", loopTracker, ",", currentBand);
-
-}
-
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+	if (currentBand!==undefined) {
+		bandElement.innerHTML +=("<div>" + currentBand + "</div");
+	}
 	currentVeggie= vegetables[loopTracker];
-	console.log("Loop Number, Veggie:", loopTracker, ",", currentVeggie);
+	if (currentVeggie!==undefined) {
+		veggieElement.innerHTML +=("<div>" + currentVeggie + "</div");
+	}
 }
-
-
-
-
-  // Add the band names into the correct <div>
-  // currentBand = bandElement.innerHTML("These are boy bands: " + bands.join(", "));
-  bandElement.innerHTML = "These are boy bands: " + bands.join(", ");
-
-
-  // Add the veggie names into the correct <div>
-  // currentVeggie = ???;
-  veggieElement.innerHTML= "These are vegetables: " + vegetables.join(", ");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
